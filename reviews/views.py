c471bf99294e2a9b5cddf6209415abecdfefd427
Robin K <124767432+robin-private@users.noreply.github.com>
2023-04-13 14:54:44 +0530

    Add files via upload

---
+++ reviews/views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, ReviewSerializer, \
+    CategorySerializer, BusinessSerializer
+from .models import Review, Business, Category
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows reviews to be viewed or edited.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BusinessViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows business to be viewed or edited.
+    """
+    queryset = Business.objects.all()
+    serializer_class = BusinessSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows category to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
